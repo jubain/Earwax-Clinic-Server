aad8c33ca788ab1df38443915607a45e9acdb19d
Jubeen Amatya <45592860+jubain@users.noreply.github.com>
2022-09-20 22:14:59 +0100

    adding env variable

--- src/api.js
+++ src/api.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const serverless = require('serverless-http')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+let cors = require('cors')
+
+const stripe = require('stripe')(process.env.SECRET_KEY)
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 const router = express.Router()
 
-router.get('/', function (req, res) {
-    res.json({
-        'hello': 'hi'
-    })
+router.get('/', async function (req, res) {
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 3000,
+        currency: 'gbp',
+        automatic_payment_methods: { enabled: true },
+        receipt_email: 'jubeennp@gmail.com',
+    });
+    res.json({ client_secret: paymentIntent.client_secret })
 })
 app.use('/.netlify/functions/api', router)
 module.exports = app
