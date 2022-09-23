4497290e45f43189ebf9f2bc41bc050d55ee15b1
Jubeen Amatya <45592860+jubain@users.noreply.github.com>
2022-09-23 22:00:57 +0100

    fixed

--- src/api.js
+++ src/api.js
@@ -17,12 +17,24 @@ let corsOptions = {
 app.use(cors())
 router.post('/', async function (req, res) {
     var userDetails = JSON.parse(req.body.toString());
-
-    const customer = await stripe.customers.create({
-        email: userDetails.email,
-        name: userDetails.fullName,
+    // CHECK IF CUSTOMER EXIST
+    const customerExist = await stripe.customers.search({
+        query: `email:\'${userDetails.email}\'`,
     });
-   
+
+    // IF DOES NOT EXIST CREATE A NEW
+    let customer
+    if (customerExist.data.length === 0) {
+        customer = await stripe.customers.create({
+            email: userDetails.email,
+            name: userDetails.fullName,
+        });
+        // console.log(customer)
+    } else {
+        // IF EXIST 
+        customer = customerExist.data[0]
+    }
+
     const paymentIntent = await stripe.paymentIntents.create({
         amount: 50,
         currency: 'gbp',
